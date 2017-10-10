981880f22d5886620f0956f6c48d2aec2b97edf2
Quang Nguyen <quang.nguyen@launchdeckpowergate.com>
2017-10-10 09:40:36 +0700

    Add bookshelf and re-structure project

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const Hapi = require('hapi');
 const Good = require('good');
 const server = new Hapi.Server();
 
-server.connection({ port: process.env.PORT || 3000, host: 'localhost' });
+server.connection({ port: process.env.PORT || 3001, host: 'localhost' });
 
 const routeConfig = {
   state: {
