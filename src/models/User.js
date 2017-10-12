78cb564b940b3ec8ffe6699cb121342e5d35d9b2
Le Anh Tuan <tuanleanh1994@gmail.com>
2017-10-12 09:46:10 +0700

    user model

--- src/models/User.js
+++ src/models/User.js
@@ -1,2 +1,27 @@
 //Use bookshelf here
 //https://www.npmjs.com/package/bookshelf
+var bookshelf = require('../tools/bookShelfConfig');
+
+var User = bookshelf.Model.extend({
+    tableName: 'users',
+    hasTimestamps: true    
+});
+
+var Users = bookshelf.Collection.extend({
+    model: User
+})
+// class Users extends bookshelf.Model {
+//     get tableName() {
+//         return 'users';
+//     }
+    
+//     get hasTimestamps() {
+//         return true;
+//     }
+
+// }
+
+module.exports = {
+    User: User,
+    Users: Users
+}
